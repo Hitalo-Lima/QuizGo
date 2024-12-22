8f4ba72338797230b0821f3c4e3ba72ff80bf7af
Hitalo-Lima <hitalolima173@gmail.com>
2024-12-21 22:03:16 -0300

    feat: leitura do arquivo csv e inicializando estado do jogo

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"encoding/csv"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 type Question struct {
@@ -33,7 +35,47 @@ func (g *GameState) Init() {
 	fmt.Printf("Vamos ao jogo %s", g.Name)
 }
 
+func (g *GameState) ProccessCSV() {
+	file, err := os.Open("./quiz.csv")
+	if err != nil {
+		panic("Erro ao ler arquivo csv")
+	}
+
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		panic("Erro ao ler csv")
+	}
+
+	for i, record := range records {
+		fmt.Println(i, record)
+
+		if i > 0 {
+			question := Question{
+				Text:    record[0],
+				Options: record[1:5],
+				Answer:  toInt(record[5]),
+			}
+
+			g.Questions = append(g.Questions, question)
+		}
+	}
+}
+
+func toInt(str string) int {
+	i, err := strconv.Atoi(str)
+	if err != nil {
+		panic(err)
+	}
+
+	return i
+}
+
 func main() {
 	game := &GameState{}
+	go game.ProccessCSV()
 	game.Init()
+	fmt.Println(game)
 }
